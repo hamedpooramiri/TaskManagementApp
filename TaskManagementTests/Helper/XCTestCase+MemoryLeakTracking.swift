407b75f72ce3a2082c2f745c5e86d7e9a5726076
hamed <hamedpooramiri1007@gmail.com>
2023-10-31 21:20:50 +0330

    complete implementing trackForMemoryLeaks

--- TaskManagementTests/Helper/XCTestCase+MemoryLeakTracking.swift
+++ TaskManagementTests/Helper/XCTestCase+MemoryLeakTracking.swift
@@ -5,4 +5,12 @@
 //  Created by hamedpouramiri on 10/31/23.
 //
 
-import Foundation
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. potential memory leak.", file: file, line: line)
+        }
+    }
+}
