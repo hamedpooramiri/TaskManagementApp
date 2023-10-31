0c95a552cb403dabb4329bf74ebedb7cfe6fdedf
hamed <hamedpooramiri1007@gmail.com>
2023-10-31 22:13:40 +0330

    fix typo in DeleteTaskFromCacheUseCaseTests

--- TaskManagementTests/Cache/LocalTaskLoader/DeleteTaskFromCacheUseCaseTests.swift
+++ TaskManagementTests/Cache/LocalTaskLoader/DeleteTaskFromCacheUseCaseTests.swift
@@ -1,5 +1,5 @@
 //
-//  DeleteTaskToCacheUseCaseTests.swift
+//  DeleteTaskFromCacheUseCaseTests.swift
 //  TaskManagementTests
 //
 //  Created by hamedpouramiri on 10/31/23.
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class DeleteTaskToCacheUseCaseTests: XCTestCase {
+final class DeleteTaskFromCacheUseCaseTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
