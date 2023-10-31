445a81265bb34ff023e79e007a794114043e0ddd
hamed <hamedpooramiri1007@gmail.com>
2023-10-31 23:41:09 +0330

    create file TaskStoreSpecs, XCTestCase+TaskStoreSpecs

---
+++ TaskManagementTests/Cache/TaskStore/TaskStoreSpecs.swift
@@ -0,0 +1,8 @@
+//
+//  TaskStoreSpecs.swift
+//  TaskManagementTests
+//
+//  Created by hamedpouramiri on 10/31/23.
+//
+
+import Foundation
---
+++ TaskManagementTests/Cache/TaskStore/XCTestCase+TaskStoreSpecs.swift
@@ -0,0 +1,8 @@
+//
+//  XCTestCase+TaskStoreSpecs.swift
+//  TaskManagementTests
+//
+//  Created by hamedpouramiri on 10/31/23.
+//
+
+import Foundation
