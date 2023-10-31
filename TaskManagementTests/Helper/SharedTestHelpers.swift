affe406d4d8f0168d2523967b9e41f6223d6b501
hamed <hamedpooramiri1007@gmail.com>
2023-10-31 21:39:21 +0330

    create file SharedTestHelper

---
+++ TaskManagementTests/Helper/SharedTestHelpers.swift
@@ -0,0 +1,12 @@
+//
+//  SharedTestHelpers.swift
+//  TaskManagementTests
+//
+//  Created by hamedpouramiri on 10/31/23.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    NSError(domain: "any error", code: 0)
+}
